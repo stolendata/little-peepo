47505b09ed8866ccbc448fc2b50cb803ee09b84a
stolendata <djinn@stolendata.net>
2023-01-14 20:49:18 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+A crude and simplistic POP3 server
+==================================
+Copyright © 2009 Robin Leffmann \<djinn \[at\] stolendata.net>
+
+No mbox support. No SASL support. No nothing. Requires only [IO::Socket::SSL](https://metacpan.org/pod/IO::Socket::SSL) and [Net::SSLeay](https://metacpan.org/pod/Net::SSLeay). Accounts can be reloaded at run-time by sending SIGHUP. 
+
+License
+-------
+Licensed under Creative Commons BY-SA 4.0 - https://creativecommons.org/licenses/by-sa/4.0/
---
+++ little_peepo.pl
@@ -0,0 +1,365 @@
+#!/usr/bin/perl
+
+# little peepo v0.1 - https://github.com/stolendata/little-peepo
+#
+# (c) 2009 robin leffmann <djinn at stolendata dot net>
+#
+#   * crude and simplistic POP3 server
+#   * speaks only implicit TLS (POP3S), no STARTTLS or unencrypted chatter
+#   * can only interact with Maildir, not mbox
+#   * understands APOP and USER/PASS auth but not SASL
+#   * is your fren and tries hard but may still fail
+#
+# licensed under CC BY-SA 4.0 - https://creativecommons.org/licenses/by-sa/4.0/
+
+use strict;
+use warnings;
+
+use Digest::MD5 'md5_hex';
+use File::Basename 'basename';
+use IO::Socket::SSL;
+
+use constant DEBUG=>1;
+use constant MAX_CLIENTS=>5;
+use constant AUTH_GRACE_SEC=>5;
+use constant CLIENT_TIMEOUT_SEC=>10;
+use constant ERRS_BEFORE_KICK=>10;
+use constant SSL_TLS_VERSIONS=>'!SSLv23:!SSLv2:!SSLv3:!TLSv1:!TLSv11';
+use constant LISTEN_IP=>'0.0.0.0';
+use constant LISTEN_PORT=>995;
+use constant ACCOUNTS_FILE=>'./peepos.conf'; # here be POP3 accounts/passwords
+
+# {U} expands to the local username of the account
+use constant MAILDIR=>'/var/mail/{U}';
+
+# domains served: 'domain.name'=>'full-chain-and-privkey.pem'
+use constant DOMAINS=>{ 'some.domain.name'=>'./peepo.pem' };
+
+my ( $master, $peepos, $reload, $clients, $errs ) = ( $$, undef, 1, 0, 0 );
+
+$SIG{HUP} = sub { $reload = 1; };
+$SIG{CHLD} = sub { wait; $clients--; };
+
+my $sock = IO::Socket::INET->new( LocalAddr=>LISTEN_IP, LocalPort=>LISTEN_PORT,
+                                  Proto=>'tcp', Listen=>MAX_CLIENTS,
+                                  ReuseAddr=>1, ReusePort=>1 ) or die $!;
+
+blog( 'little peepo is accepting connections on port ' . LISTEN_PORT );
+
+while ( 1 )
+{
+    if ( $reload )
+    {
+        $reload = 0;
+        $peepos = do ACCOUNTS_FILE;
+        my ( $domains, $accounts ) = ( scalar keys %{DOMAINS()}, 0 );
+        $accounts += scalar keys %{$peepos->{$_}} for keys %{DOMAINS()};
+        blog( "serving $accounts accounts in $domains domains" );
+    }
+
+    my $c = $sock->accept or next;
+
+    if ( $clients >= MAX_CLIENTS )
+    {
+        $c->shutdown( IO::Socket::SHUT_RDWR );
+        blog( $c->peerhost . " wanted to connect, denied since we're full" );
+        next;
+    }
+
+    if ( my $pid = fork )
+    {
+        $clients++;
+        blog( 'new connection from ' . $c->peerhost . " forked to pid $pid" );
+        next;
+    }
+
+    IO::Socket::SSL->start_SSL( $c, Timeout=>5, SSL_server=>1,
+                                SSL_version=>SSL_TLS_VERSIONS,
+                                SSL_cert_file=>DOMAINS );
+
+    if ( $SSL_ERROR )
+    {
+        $c->shutdown( IO::Socket::SHUT_RDWR );
+        blog( "disconnecting because $SSL_ERROR" );
+        exit;
+    }
+
+    my ( $conn_start, $txphase, $mailbox, $maildir ) = ( time, 0, {}, undef );
+    my ( $account, $user, $cmd_count ) = ( undef, undef, 0 );
+
+    # APOP auth banner
+    srand();
+    my $domain = $c->get_servername;
+    my $banner = '<';
+    $banner .= ( 'a'..'z', 'A'..'Z' )[rand(52)] for 1..8;
+    $banner .= "$$.$conn_start\@$domain>";
+
+    blog( "connected to $domain with " . $c->get_sslversion );
+    print $c "+OK little peepo is ready $banner\r\n";
+
+    while ( $c->connected )
+    {
+        blog( 'kicking noisy client' ), last if $errs >= ERRS_BEFORE_KICK;
+
+        if ( $txphase == 0 and time - $conn_start >= AUTH_GRACE_SEC )
+        {
+            blog( 'kicking stalling client' );
+            last;
+        }
+
+        my ( $buf, $rb );
+        eval
+        {
+            local $SIG{ALRM} = sub { die "timed out\n" };
+            alarm CLIENT_TIMEOUT_SEC;
+            $rb = read( $c, $buf, 128 );
+            alarm 0;
+        };
+
+        # client stalled or connection dropped
+        if ( $@ or !$rb )
+        {
+            blog( "DEBUG connection hiccup, $!" ) if ( DEBUG and $! );
+
+            blog( "timed out after sending $cmd_count commands" ) if $@;
+            err( $c, 'taking too long' ) if $@;
+
+            blog( "dropped after sending $cmd_count commands" ) unless $@;
+
+            last;
+        }
+
+        # auth phase
+
+        $buf =~ s/(\r?\n.*|[^a-z0-9 @._-]|^\s+|\s+$)//gi;
+        my @p = split( /\s+/, $buf );
+        my $cmd = uc ( $p[0] // '' );
+
+        blog( "DEBUG \"$buf\"" ) if DEBUG;
+
+        $cmd_count++ if length $cmd;
+
+        if ( $cmd eq 'QUIT' )
+        {
+            blog( "quit after sending $cmd_count commands" );
+            print $c "+OK bye\r\n";
+            last;
+        }
+        elsif ( $cmd eq 'CAPA' )
+        {
+            print $c "+OK little peepo spellbook\r\n";
+            print $c "$_\r\n" for ( 'IMPLEMENTATION little-peepo-v0.1',
+                                    'LOGIN-DELAY 120', 'EXPIRE 0',
+                                    'USER', 'UIDL', 'TOP', '.' );
+        }
+        elsif ( $txphase == 0 )
+        {
+            if ( $cmd eq 'APOP' and length $p[1] and length $p[2] )
+            {
+                my $hash = defined $peepos->{$domain}{$p[1]}
+                           ? md5_hex( $banner . $peepos->{$domain}{$p[1]}[0] )
+                           : '';
+
+                if ( $p[2] eq $hash )
+                {
+                    $account = $p[1];
+                    $user = $peepos->{$domain}{$account}[1];
+                    $txphase = 1;
+                }
+                else
+                {
+                    err( $c, 'nope' );
+                }
+            }
+            elsif ( $cmd eq 'USER' and length $p[1] )
+            {
+                $account = $p[1];
+                print $c "+OK go on\r\n";
+            }
+            elsif ( $cmd eq 'PASS' and length $p[1] and length $account )
+            {
+                if ( defined $peepos->{$domain}{$account}
+                     and $p[1] eq $peepos->{$domain}{$account}[0] )
+                {
+                    $user = $peepos->{$domain}{$account}[1];
+                    $txphase = 1;
+                }
+                else
+                {
+                    err( $c, 'nope' );
+                    $account = '';
+                }
+            }
+            else
+            {
+                err( $c, 'uhh' );
+            }
+
+            # credentials matched
+            if ( $txphase )
+            {
+                $maildir = MAILDIR =~ s/\{U\}/$user/gr;
+                my ( $uid, $gid ) = ( getpwnam($user) )[2, 3];
+
+                my $j = ( $uid // 0 ) ? chroot( $maildir ) : 0;
+                $j = chdir( '/' ) if ( $j and $uid and $gid );
+
+                if ( $j )
+                {
+                    $( = $gid; $) = $gid; $< = $uid; $> = $uid;
+                    $( = $gid; $) = $gid; $< = $uid; $> = $uid;
+                }
+
+                # jailing and/or privilege drop failed
+                if ( !$j or $> != $uid or ($) =~ /(^\d+)/)[0] != $gid )
+                {
+                    blog( "mailbox $maildir for $account inaccessible" );
+                    err( $c, 'mailbox not found' );
+                    last;
+                }
+
+                blog( "DEBUG chrooted with privs $>:$)" ) if DEBUG;
+                tally_mailbox( $mailbox, $domain );
+                blog( "$account authenticated for mailbox $maildir" );
+                blog( "found $mailbox->{count} messages for $account" );
+
+                print $c "+OK little peepo welcomes you\r\n";
+            }
+        }
+        #
+        # transaction phase
+        #
+        elsif ( $cmd eq 'STAT' )
+        {
+            print $c "+OK $mailbox->{count} $mailbox->{bytes}\r\n";
+        }
+        elsif ( $cmd eq 'LIST' or $cmd eq 'UIDL' )
+        {
+            blog( "DEBUG found $mailbox->{count} messages" ) if DEBUG;
+
+            my $count = $mailbox->{count};
+
+            if ( $count == 0 )
+            {
+                print $c "+OK nothing here\r\n.\r\n" if $cmd eq 'LIST';
+                err( $c, 'nothing here' ) if $cmd eq 'UIDL';
+                next;
+            }
+
+            if ( length $p[1] )
+            {
+                my $num = $p[1] =~ tr/0-9//cdr || 0; # YES, THANK YOU, I KNOW
+
+                if ( defined $mailbox->{msgs}{$num} )
+                {
+                    my $field = $cmd eq 'LIST' ? 'bytes' : 'uid';
+                    print $c "+OK $num $mailbox->{msgs}{$num}{$field}\r\n";
+                }
+                else
+                {
+                    err( $c, 'no such message' );
+                }
+            }
+            else
+            {
+                my $field = $cmd eq 'LIST' ? 'bytes' : 'uid';
+                print $c "+OK $count messages\r\n";
+                print $c "$_ $mailbox->{msgs}{$_}{$field}\r\n" for 1..$count;
+                print $c ".\r\n";
+            }
+        }
+        elsif ( $cmd eq 'RETR' and length $p[1] )
+        {
+            my $num = $p[1] =~ tr/0-9//cdr || 0;
+
+            if ( !defined $mailbox->{msgs}{$num} )
+            {
+                err( $c, 'no such message' );
+                next;
+            }
+
+            my $file = $mailbox->{msgs}{$num}{file};
+            my $mail = "From: little peepo\r\nTo: you\r\n\r\nmail fail ;(";
+            open( my $fh, '<', "./new/$file" );
+            { local $/; $mail = <$fh>; };
+            close( $fh );
+
+            print $c "+OK $mailbox->{msgs}{$num}{bytes} bytes\r\n";
+            print $c $mail;
+            print $c "\r\n.\r\n";
+
+            my $newfile = $file =~ /:2,$/ ? "${file}S" : "$file:2,S";
+            $mailbox->{msgs}{$num}{file} = $newfile;
+            blog( "RETR $file -> $newfile" );
+            rename( "./new/$file", "./cur/$newfile" );
+        }
+        elsif ( $cmd eq 'DELE' and length $p[1] )
+        {
+            my $num = $p[1] =~ tr/0-9//cdr || 0;
+
+            if ( !defined $mailbox->{msgs}{$num} )
+            {
+                err( $c, 'no such message' );
+                next;
+            }
+
+            my $file = $mailbox->{msgs}{$num}{file};
+            my $newfile = $file =~ s/(?::2,[^T]?)?$/:2,T/r;
+            blog( "DELE $file -> $newfile" );
+            rename( "./cur/$file", "./cur/$newfile" );
+
+            print $c "+OK poof\r\n";
+        }
+        elsif ( $cmd eq 'NOOP' or $cmd eq 'RSET' )
+        {
+            print $c "+OK\r\n";
+        }
+        else
+        {
+            err( $c, 'uhh' );
+        }
+    }
+
+    $c->close;
+    last;
+}
+
+sub blog
+{
+    my ( $msg ) = @_;
+
+    printf( "%i [%s]: %s\n", (time, ($$ == $master ? 'master' : $$), $msg) );
+}
+
+sub err
+{
+    my ( $c, $msg ) = @_;
+
+    $errs++;
+    sleep 1;
+    print $c "-ERR $msg\r\n";
+}
+
+sub tally_mailbox
+{
+    my ( $mailbox, $domain ) = @_;
+
+    undef $mailbox->{msgs};
+    $mailbox->{count} = $mailbox->{bytes} = 0;
+
+    for ( glob('./new/*') )
+    {
+        next unless ( -f $_ and -r $_ and $_ !~ /:2,.$/ and (my $fs = -s $_) );
+
+        $domain =~ s/[.]/-/g;
+        my $file = basename( $_ );
+        my ( $uid_domain ) = $file =~ /^.+?[.].+?[.]([^,]+),?/;
+        my $uid = $file =~ s/$uid_domain.*$/$domain/r;
+
+        $mailbox->{count}++;
+        $mailbox->{bytes} += $fs;
+        $mailbox->{msgs}{$mailbox->{count}}{file} = $file;
+        $mailbox->{msgs}{$mailbox->{count}}{uid} = $uid;
+        $mailbox->{msgs}{$mailbox->{count}}{bytes} = $fs;
+    }
+}
